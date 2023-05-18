0a936144d5e160ec92e025bb87a08bb2aa4af9aa
Dari <pinobobino@gmail.com>
2023-05-18 12:59:39 +0300

    Исправление кода

--- script.js
+++ script.js
@@ -13,72 +13,70 @@ const sliderItem = document.querySelectorAll('.slider-item'),
 let sliderCount = 0,
   sliderWidth
 
-// Элементы клика
+if (document.documentElement.clientWidth > 1200) {
+  // Элементы клика
 
-arrowLeft.addEventListener('click', prevSlide)
-arrowRight.addEventListener('click', nextSlide)
+  arrowLeft.addEventListener('click', prevSlide)
+  arrowRight.addEventListener('click', nextSlide)
 
-setInterval(() => {
-  nextSlide()
-}, 4000)
+  setInterval(() => {
+    nextSlide()
+  }, 4000)
 
-// Задаем нужный размер карточке
-
-// Слайд вперед
-function nextSlide() {
-  sliderCount++
-  if (sliderCount >= sliderImg.length) sliderCount = 0
-  thisSlide(sliderCount)
-}
-
-// Слайд назад
-function prevSlide() {
-  sliderCount--
-  if (sliderCount < 0) sliderCount = sliderImg.length - 1
-  thisSlide(sliderCount)
-}
-
-// Шаг перемещения слайда
-function thisSlide(index) {
-  sliderLine.forEach((item) => item.classList.remove('line_active'))
-  sliderLine[index].classList.add('line_active')
-  sliderItem.forEach((item) => item.classList.remove('slider-item_active'))
-  sliderItem[index].classList.add('slider-item_active')
-}
-
-// Клик на line
-
-sliderLine.forEach((line, index) => {
-  line.addEventListener('click', () => {
-    sliderCount = index
-    thisSlide(sliderCount)
-  })
-})
-sliderLineFooter.forEach((line, index) => {
-  line.addEventListener('click', () => {
-    sliderCount = index
-    thisSlide(sliderCount)
-  })
-})
-// Поведение для экранов < 1200 px
-
-if (document.documentElement.clientWidth <= 1200) {
-  arrowLeftFooter.addEventListener('click', prevSlide)
-  arrowRightFooter.addEventListener('click', nextSlide)
-
-  // setInterval(() => {
-  //   nextSlide()
-  // }, 4000)
+  // Задаем нужный размер карточке
 
   // Слайд вперед
   function nextSlide() {
+    sliderCount++
+    if (sliderCount >= sliderImg.length) sliderCount = 0
+    thisSlide(sliderCount)
+  }
+
+  // Слайд назад
+  function prevSlide() {
+    sliderCount--
+    if (sliderCount < 0) sliderCount = sliderImg.length - 1
+    thisSlide(sliderCount)
+  }
+
+  // Шаг перемещения слайда
+  function thisSlide(index) {
+    sliderLine.forEach((item) => item.classList.remove('line_active'))
+    sliderLine[index].classList.add('line_active')
+    sliderItem.forEach((item) => item.classList.remove('slider-item_active'))
+    sliderItem[index].classList.add('slider-item_active')
+  }
+
+  // Клик на line
+
+  sliderLine.forEach((line, index) => {
+    line.addEventListener('click', () => {
+      sliderCount = index
+      thisSlide(sliderCount)
+    })
+  })
+  sliderLineFooter.forEach((line, index) => {
+    line.addEventListener('click', () => {
+      sliderCount = index
+      thisSlide(sliderCount)
+    })
+  })
+}
+// Поведение для экранов < 1200 px
+
+if (document.documentElement.clientWidth <= 1200) {
+  arrowLeftFooter.addEventListener('click', prevSlideMobile)
+  arrowRightFooter.addEventListener('click', nextSlideMobile)
+
+  // Слайд вперед
+  function nextSlideMobile() {
     sliderCount++
     if (sliderCount >= sliderImg.length) sliderCount = 0
     thisSlideMobile(sliderCount)
   }
 
   // Слайд назад
-  function prevSlide() {
+  function prevSlideMobile() {
     sliderCount--
     if (sliderCount < 0) sliderCount = sliderImg.length - 1
     thisSlideMobile(sliderCount)
@@ -91,8 +89,8 @@ if (document.documentElement.clientWidth <= 1200) {
     )
     sliderLineFooter[index].classList.add('footer__line_active')
     sliderItem.forEach((item) => item.classList.remove('slider-item_active'))
-    sliderItem[index].classList.add('slider-item_active')
     sliderItem.forEach((item) => item.classList.remove('showing'))
+    sliderItem[index].classList.add('slider-item_active')
     sliderItem[index].classList.add('showing')
   }
 }
